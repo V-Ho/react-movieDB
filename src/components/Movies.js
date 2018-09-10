bed6f9030880fcc23ad55a8f46fab8b261d93ee5
Vanessa Ho <vanessa.lg.ho@gmail.com>
2018-09-10 17:34:50 +0200

    fetch movie api data

--- src/components/Movies.js
+++ src/components/Movies.js
@@ -2,8 +2,17 @@ import React from 'react'
 import MovieListItem from './MovieListItem'
 
 const titles = ['Beauty & the Beast', 'Nemo', 'Wall-E']
+const API_KEY = process.env.REACT_APP_movie_api_key
 
 class Movies extends React.Component {
+  componentDidMount () {
+    const apiURL = `https://api.themoviedb.org/3/discover/movie?api_key=${API_KEY}&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1`
+
+    fetch(apiURL)
+      .then(res => res.json())
+      .then(data => console.log(data))
+      .catch(err => console.log(err))
+  }
   render () {
     return (
       <ul className='movies'>
