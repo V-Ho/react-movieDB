24688519c0564696edd1d10e3120e33ef56ad097
Vanessa Ho <vanessa.lg.ho@gmail.com>
2018-09-11 17:39:27 +0200

    store movie data in state

--- src/components/Movies.js
+++ src/components/Movies.js
@@ -1,24 +1,44 @@
 import React from 'react'
 import MovieListItem from './MovieListItem'
 
-const titles = ['Beauty & the Beast', 'Nemo', 'Wall-E']
+// const titles = ['Beauty & the Beast', 'Nemo', 'Wall-E']
 const API_KEY = process.env.REACT_APP_movie_api_key
 
 class Movies extends React.Component {
+  constructor (props) {
+    super(props)
+    this.state = {
+      movies: []
+    }
+    this.storeMovies = this.storeMovies.bind(this)
+  }
   componentDidMount () {
     const apiURL = `https://api.themoviedb.org/3/discover/movie?api_key=${API_KEY}&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1`
 
     fetch(apiURL)
       .then(res => res.json())
-      .then(data => console.log(data))
+      // .then(data => console.log(data))
+      .then(data => this.storeMovies(data))
       .catch(err => console.log(err))
   }
+
+  // assign results of fetch movies, then map and store data in state
+  storeMovies (data) {
+    const movies = data.results.map(res => {
+      const { vote_count, id, genre_ids, poster_path, title, vote_average, release_date } = res
+
+      return { vote_count, id, genre_ids, poster_path, title, vote_average, release_date }
+    })
+
+    this.setState({ movies })
+  }
+
   render () {
     return (
       <ul className='movies'>
         {
-          titles.map((title, index) =>
-            <MovieListItem key={index.toString()} title={title} />
+          this.state.movies.map((movie) => // map over this.state.movies array, pass in movie object
+            <MovieListItem key={movie.id} movie={movie} />
           )
         }
       </ul>
